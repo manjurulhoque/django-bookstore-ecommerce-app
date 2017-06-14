a30387dd28a1586cedbd14be2b3f2378d36bfc0d
manjurulhoque <rumimanzurulhoque@gmail.com>
2017-06-15 03:46:07 +0600

    added registration

--- store/views.py
+++ store/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.views.generic import View
 
 # Create your views here.
 
 from .models import Book
+from .forms import UserForm
 
 
 def index(request):
@@ -15,3 +18,23 @@ def store(request):
         'count': count
     }
     return render(request, 'store.html', context)
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('/')
+    return render(request, 'index.html', {})
